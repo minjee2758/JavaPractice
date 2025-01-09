be4ed89e5f6b814ca47d6dd0eb4848af4cd98959
minjee2758 <minjee2758@naver.com>
2025-01-09 17:32:41 +0900

    Java Section3

---
+++ javaPractice/src/CommentJava.java
@@ -0,0 +1,7 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hellojava1을 무시하고 출력합니다
+        //System.out.println("glgl");
+        /*System.out.println("dd");*/
+    }
+}
---
+++ javaPractice/src/variable/Var5.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a);
+
+        int b = 2; //선언과 초기화를 한 번에
+        System.out.println(b);
+    }
+}
