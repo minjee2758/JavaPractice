029910f1220a81d6eeb27959a0654a9c2771dcf0
minjee2758 <minjee2758@naver.com>
2025-01-09 22:15:09 +0900

    Java Section4

---
+++ javaPractice/src/operator/Operator1.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int sum = a+b;
+        System.out.println("a + b = " + sum);
+
+        int diff = a - b;
+        System.out.println(diff);
+
+        int div = a/b;
+        System.out.println(div);
+
+        int mod = a%b;
+        System.out.println(mod);
+    }
+}
